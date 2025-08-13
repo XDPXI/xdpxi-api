598a70a43b51ec7c7cbea9296e0b96c96f086e04
Xavier Perri <xdpxigaming@gmail.com>
2025-08-13 13:03:19 +1000

    Add Everything

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+# =============== UNITED EMPIRES ===============
+
+@app.route("/ue/v1/server/status", methods=["GET"])
+def server_status():
+    request = requests.get("https://mcstatus.xdpxi.dev/api/v1/ue.xdpxi.net:59280")
+
+    if request.status_code == 200:
+        return request.json()
+
+    return 'Server status unresponsive', 500
+
+@app.route("/ue/v1/server/start", methods=["POST"])
+def start_server():
+    return 'Work in progress', 501
+
+@app.route("/ue/v1/server/stop", methods=["POST"])
+def stop_server():
+    return 'Work in progress', 501
+
+@app.route("/ue/v1/server/restart", methods=["POST"])
+def restart_server():
+    return 'Work in progress', 501
+
+# =============== XDPXI ===============
+
+# something idk
+
+# =====================================
+
+if __name__ == "__main__":
+    app.run(debug=True)
